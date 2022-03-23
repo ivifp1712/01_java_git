5c0a7f2f630c024171d4183f335c3900e4b18f6d
Ivan Gonzalez <ivan.gonzalez@campusfp.es>
2022-03-23 10:15:18 +0100

    segundo commit

--- src/com/ejemplo/teoria/Prueba.java
+++ src/com/ejemplo/teoria/Prueba.java
@@ -4,7 +4,7 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("probando Proyecto");
+		System.out.println("probando el Proyecto");
 	}
 
 }
