b84afa4cd78364e33d4f2ebd5a6086a34b4da6f5
Ivan Gonzalez <ivan.gonzalez@campusfp.es>
2022-03-23 10:21:45 +0100

    constructor getters/setters commit

--- src/com/ejemplo/teoria/Cliente.java
+++ src/com/ejemplo/teoria/Cliente.java
@@ -4,4 +4,39 @@ public class Cliente {
 	String nombre;
 	String ciudad;
 	int factura;
+	
+	public Cliente(String nombre, String ciudad, int factura) {
+		super();
+		this.nombre = nombre;
+		this.ciudad = ciudad;
+		this.factura = factura;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	public int getFactura() {
+		return factura;
+	}
+
+	public void setFactura(int factura) {
+		this.factura = factura;
+	}
+	
+	
+	
+	
 }
