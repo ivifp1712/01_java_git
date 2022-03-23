4e95366c827c79942d5cb04e9db9ee102f1c3cf0
Ivan Gonzalez <ivan.gonzalez@campusfp.es>
2022-03-23 10:10:31 +0100

    primer commit

---
+++ src/com/ejemplo/teoria/Cliente.java
@@ -0,0 +1,7 @@
+package com.ejemplo.teoria;
+
+public class Cliente {
+	String nombre;
+	String ciudad;
+	int factura;
+}
---
+++ src/com/ejemplo/teoria/Prueba.java
@@ -0,0 +1,10 @@
+package com.ejemplo.teoria;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("probando Proyecto");
+	}
+
+}
